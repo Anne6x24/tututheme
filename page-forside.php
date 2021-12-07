cdb21a23a592d36992edf7413d09c61656216d12
“Anne6x24” <Anne6x24@stud.kea.dk>
2021-12-07 11:05:31 +0100

    hej

--- page-forside.php
+++ page-forside.php
@@ -4,3 +4,46 @@ get_header();
 get_footer();
 ?>
 
+<style>
+
+
+
+</style>
+
+<!-- <section id="primary" class="content"> -->
+    <main id="site-content">
+    <section id="toppen">
+        <img class="hero" src="" alt="HERO">
+        <div id="tekst_1">
+    <h1 id="headline" class="secondhand">Secondhand i børnehøjde</h1>
+    <p class="secondhand">Mange har et ønske om at klæde deres børn lækkert og smart på. 
+        Det kan skabe glæder hos de fleste mødre, at deres børn også ser godt ud. 
+        En ting der er sikkert med børn, er dog at de vokser og de vokser hurtigt. 
+        Derfor er der meget af deres tøj der ikke bliver brugt længe af gangen og 
+        får en plads på loftet eller i en kasse under sengen. Men er det den bedste 
+        måde at behandle tøj på, det mener vi ikke. Et lækkert stykke børnetøj, skal 
+        kunne blive brugt mange gange og give glæde og varme til så mange børn som overhovedet muligt.</p>
+        <button>Se vores vare her</button>
+        </div>
+        <div class="kjole1">
+                <img src="https://annemunksgaard.dk/kea/02sem/eksamen_tutu/tutu_wp/wp-content/uploads/2021/12/A976787-R2-34-3-e1638784792853.jpg" alt="">
+                </div>
+    </section>
+    <section id="midten">
+        <h2>GUIDE</h2>
+    </section>
+    <section id="bunden">
+        <div class="insta_h">
+        <h3>Find os på instagram</h3>
+        </div>
+        <div class="insta">
+        <a href="https://www.instagram.com/lilletutu_secondhand/">@lilletutu_secondhand</a>
+        </div>
+    </section>
+    </main>
+
+<!-- </section> -->
+<?php
+
+get_footer();
+?>
